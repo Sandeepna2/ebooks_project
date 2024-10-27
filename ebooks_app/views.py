7d0a50f1fdc4efd5a2f5b1184b5a909d9f083cf1
Sandeep <ssandeepraj00652@gmail.com>
2024-10-27 23:48:25 +0530

    Timezone set & ebooks_app created

---
+++ ebooks_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def contact(request):
+    return HttpResponse("contact us")
+
+
